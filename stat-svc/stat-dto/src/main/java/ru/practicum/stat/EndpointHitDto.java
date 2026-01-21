5ccea90b8ddc6347899024a6e8fdbc58e69079bd
IEMeniailo <menyailo96@mail.ru>
2026-01-21 16:43:27 +0300

    Все задания Этапа 2 выполнены (1)

--- stat-svc/stat-dto/src/main/java/ru/practicum/stat/EndpointHitDto.java
+++ stat-svc/stat-dto/src/main/java/ru/practicum/stat/EndpointHitDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.stat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -16,5 +17,6 @@ public class EndpointHitDto {
     String app;
     String uri;
     String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime timestamp;
 }
