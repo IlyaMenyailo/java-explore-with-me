69afabdeb74c7ee9930dc8387c99e0d321787b60
IEMeniailo <menyailo96@mail.ru>
2026-01-24 21:46:18 +0300

    Все задания Этапа 2 выполнены (5), ошибки исправлены (12)

--- ewm-main-svc/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
+++ ewm-main-svc/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.stream.Collectors;
-
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
